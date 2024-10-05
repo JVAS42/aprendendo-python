56fda3cd82668df0359c78915e4c89d77d3bf280
Joao Victor <jvasprogramando@gmail.com>
2024-10-05 20:43:39 -0300

    Corrigindo exercicio 71

--- Mundo2/exercicio071.py
+++ Mundo2/exercicio071.py
@@ -1,4 +1,23 @@
-valorDepositado = int('Valor que será sacado? R$')
+print('{:^30}'.format('BANCO'))
+
+valorSacado = int(input('Qual o valor a ser sacado: R$'))
+total = valorSacado
+notaAtual = 50
+totalNotas = 0
 
 while True:
-
+    if total >= notaAtual:
+        total = total - notaAtual
+        totalNotas = totalNotas + 1
+    else:
+        totalNotas = 0
+        if total != 0:
+            print(f'Sacou {totalNotas} nota(s) de R${notaAtual}')
+        if notaAtual == 50:
+            notaAtual = 20
+        elif notaAtual == 20:
+            notaAtual = 10
+        elif notaAtual == 10:
+            notaAtual = 1
+        else:
+            break
