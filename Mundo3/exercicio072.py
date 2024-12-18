f67cbf962d53dfc5b6175dc0151845d766b21f93
Joao Victor <jvasprogramando@gmail.com>
2024-12-18 19:52:25 -0300

    Atualizando

--- Mundo3/exercicio072.py
+++ Mundo3/exercicio072.py
@@ -1,3 +1,4 @@
+# Minha Resolução
 tuplasDeNumeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
                    'Onze', 'Doze', 'Treze', 'Quartoze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 
@@ -8,3 +9,13 @@ while True:
     else:
         print('Número informado não foi encontrado')
         break
+
+# Resolução Curso em Vídeo
+cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
+                   'Onze', 'Doze', 'Treze', 'Quartoze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+    num = int(input('Digite um número'))
+    if num >= 0 and num <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {cont[num]}')
--- Mundo3/exercicio073.py
+++ Mundo3/exercicio073.py
@@ -1,3 +1,4 @@
+# Minha Resolução
 listaDeTimes = ('Botafogo', 'Palmeiras', 'Fortaleza', 'Flamengo', 'São Paulo',
                 'Internacional', 'Bahia', 'Cruzeiro', 'Vasco da Gama', 'Atlético-MG',
                 'Grêmio', 'Criciúma', 'Bragantino', 'Juventude', 'Athletico-PR',
@@ -26,3 +27,14 @@ print('\n')
 for i in range (0, 20):
     if listaDeTimes[i] == 'Criciúma':
         print(f'Posição do {listaDeTimes[i]} na tabela é {i+1}º')
+
+# Resolução Curso em Vídeo
+times = ('Botafogo', 'Palmeiras', 'Flamengo', 'Fortaleza', 'Internacional',
+                'São Paulo', 'Corithians', 'Bahia', 'Cruzeiro', 'Vasco',
+                'Vitória', 'Atlético-MG', 'Fluminense', 'Grêmio', 'Juventude',
+                'Bragantino', 'Athletico-PR', 'Criciúma', 'Atlético-GO', 'Cuiabá')
+
+print(f'Os primeiros times: {times[0:5]}')
+print(f'Os 4 últimos times: {times[-4:]}')
+print(f'Times em ordem algabética: {sorted(times)}')
+# Chapeco não está mais na série A
