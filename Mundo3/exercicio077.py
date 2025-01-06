9349623c5b4654dc4b0611c6b1c6448c1f2d1e16
Joao Victor <jvasprogramando@gmail.com>
2025-01-06 19:09:07 -0300

    Iniciando mundo 3

--- Mundo3/exercicio077.py
+++
@@ -1,9 +0,0 @@
-palavras = ('Concole', 'Playstation', 'Xbox', 'Computador',
-            'Controle', 'Mouse', 'Teclado', 'Monitor')
-
-for p in palavras:
-    print(f'Na palavra {p.upper()} têm as vogais', end=" ")
-    for letra in p:
-        if letra.lower() in 'aeiou':
-            print(letra, end=' ')
-    print()
--- Mundo3/exercicio078.py
+++
@@ -1,8 +0,0 @@
-valores = list()
-
-for i in range (0,5):
-    valores.append(int(input(f'Informe o {i+1}º valor: ')))
-
-print(f'O valores digitados foram: {valores}')
-print(f'O maior valor digitado é {max(valores)}')
-print(f'O menor valor digitado é {min(valores)}')
