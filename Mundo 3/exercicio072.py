31ed8cc2e45177c492926a9e6c4eb65958f3a68b
Joao Victor <jvasprogramando@gmail.com>
2025-01-07 17:25:13 -0300

    Exercicio 72 feito

---
+++ Mundo 3/exercicio072.py
@@ -0,0 +1,10 @@
+tupla_de_numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
+                    'Onze', 'Doze', 'Treze', 'Quartoze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+
+numero_escolhido = int(input('Digite um número entre 0 e 20: '))
+while True:
+    if numero_escolhido > 20:
+        numero_escolhido = int(input('Número não encontrado. Tente novamente: '))
+    else:
+        print(f'O número informado foi {tupla_de_numeros[numero_escolhido]}')
+        break
