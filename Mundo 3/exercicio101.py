185900117056f948e2bf06c923c36e0786150faa
Joao Victor <jvasprogramando@gmail.com>
2025-01-30 15:40:53 -0300

    Exercicio 101 feito

---
+++ Mundo 3/exercicio101.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+def maior_idade(a):
+    idade = datetime.now().year - a
+    if idade < 18:
+        return False
+    else:
+        return True
+
+
+ano = int(input('Em que ano você nasceu? '))
+if maior_idade(ano) == False:
+    print(f'Com {datetime.now().year - ano} anos: NÃO VOTA')
+else:
+    print(f'Com {datetime.now().year - ano} anos: VOTA')
